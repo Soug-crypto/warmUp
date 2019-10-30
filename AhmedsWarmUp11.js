abd7743b9b2156ea14691394e5c330c5fa596e36
Ahmed Wheida <ahmed.wheida@gmail.com>
2019-10-30 16:51:14 +0100

    i updated the jquery part

---
+++ AhmedsWarmUp11.js
@@ -0,0 +1,10 @@
+
+$('#leftBtn').on("click", function(){
+	$("#ulLeft").append("<li>Another one on the Left</li>")
+
+})
+
+$("#rightBtn").on("click", function(){
+	$("#ulRight").append("<li>Another one on the right</li>")
+
+})
