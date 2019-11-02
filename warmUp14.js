e181847eeac17631dd763c67f10943ff382ebcbe
Ahmed Wheida <ahmed.wheida@gmail.com>
2019-11-02 09:55:30 +0100

    still a small bug

--- warmUp14.js
+++ warmUp14.js
@@ -1,7 +1,8 @@
 /*
 Say you have an array for which the i element is the price of a given stock on day i.
 
-If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), 
+design an algorithm to find the maximum profit.
 
 Note that you cannot sell a stock before you buy one.
 
@@ -18,6 +19,38 @@ Output: 0
 Explanation: In this case, no transaction is done, i.e. max profit = 0.
 */
 
+
+//find the minimum price with array.reduce()
+// if min is the last day (i === array.length-1), return max profit = 0
+//if not, compare to the rest of the week (because you cannot buy today and sell the stock yesterday)
+
 var maxProfit = function(prices) {
-  // YOUR CODE HERE
+
+	var min, max=0;
+
+	var Maximum = max - min
+
+	prices.reduce(function(elem){
+		if(elem===Math.min(...prices)){
+			return min = elem;
+		}
+
+	})
+
+	if (prices.indexOf(min)=== array.length-1) {
+		return "Maximum prrofit is: 0";
+	} 
+
+
+	for (var i = prices.indexOf(min); i < prices.length; i++) {
+
+		if (prices[i]>max){
+
+			max = prices[i]
+		}
+
+	}
+
+	return "Maximum profit is: " + Maximum
+
 };
