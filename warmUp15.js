26118699860d1777c6a3627acd212e6444ccc532
Ahmed Wheida <ahmed.wheida@gmail.com>
2019-11-05 09:51:03 +0100

    I finished it with two for loop. I am not statisfied, but it works

--- warmUp15.js
+++ warmUp15.js
@@ -3,4 +3,32 @@
 // Example : reverseStr("hi there how is the weather today") ==>> "today weather the is how there hi"
 //
 //
-// Note : you can`t use any built-in functions .
+// Note : you can`t use any built-in functions 
+
+
+function reverseStr (str) {
+
+	var indexBengin = str.length, indexEnd = str.length, acc = "";
+
+	for (var i = str.length - 1; i >= 0; i--) {
+		if (str[i] === " " || i === 0){
+
+			indexBegin = i;
+
+			if (i===0) {
+				acc = acc + " ";
+			}
+
+			for (var j = indexBegin; j < indexEnd; j++) {
+				acc = acc + str[j]
+			}
+
+			indexEnd = i
+		}
+	}
+
+	return acc
+
+}
+
+
